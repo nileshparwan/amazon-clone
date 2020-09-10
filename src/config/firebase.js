68d9ba9e0ef0f9f29cb182cd9e10d53ee61b4f32
Parwan Koshal <parwan.koshal@hangarww.com>
2020-09-10 20:53:37 +0400

    [amazon] add firebase function to auth user

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyACl-UbKr22xPxGveKvJQru91uzKPp19TE",
     authDomain: "fir-82e13.firebaseapp.com",
@@ -7,3 +9,9 @@ const firebaseConfig = {
     messagingSenderId: "496860980487",
     appId: "1:496860980487:web:7627524c258f39148f11a2"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
