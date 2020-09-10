14aac4b2db0a2e3efd82b623adbac4eefca370cc
Parwan Koshal <parwan.koshal@hangarww.com>
2020-09-10 19:16:51 +0400

    [amazon-clone] add login route

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Checkout from './components/checkout/Checkout';
+import Login from './components/login/Login';
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
+
 
         <Switch>
 
+          <Route path="/login">
+            <Login />
+          </Route>
+
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
 
           <Route path="/">
+            <Header />
             <Home />
           </Route>
 
