d633f756df4e7c9fce405ea908dbb47fc08232bb
Parwan Koshal <parwan.koshal@hangarww.com>
2020-09-10 14:02:01 +0400

    [amazon-clone] add checkout

---
+++ src/components/checkout/Checkout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Checkout.css';
+import Subtotal from '../subtotal/Subtotal';
+import { useStateValue } from '../../stateProvider/stateProvider';
+import Product from '../product/Product';
+
+const Checkout = () => {
+    const [{ basket }, dispatch] = useStateValue();
+
+    return (
+        <div className="checkout">
+            <div className="checkout__left">
+                <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="ads" />
+
+                <div>
+                    <h2 className="checkout__title">Your shopping basket</h2>
+                    {
+                        basket.map((item, index) => <Product {...item}/>)
+                    }
+                </div>
+            </div>
+
+            <div className="checkout__right">
+                <Subtotal basket={basket} />
+            </div>
+        </div>
+    );
+};
+
+export default Checkout;
