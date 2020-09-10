903d782aad544910ecfc85ff4ae841be029b77f0
Parwan Koshal <parwan.koshal@hangarww.com>
2020-09-10 14:00:59 +0400

    [amazon-clone]  add fire base config

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyACl-UbKr22xPxGveKvJQru91uzKPp19TE",
+    authDomain: "fir-82e13.firebaseapp.com",
+    databaseURL: "https://fir-82e13.firebaseio.com",
+    projectId: "fir-82e13",
+    storageBucket: "fir-82e13.appspot.com",
+    messagingSenderId: "496860980487",
+    appId: "1:496860980487:web:7627524c258f39148f11a2"
+};
