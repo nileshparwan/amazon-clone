5b606ecd57bef3f9a07a13105ba2b0646ae0ee42
Parwan Koshal <parwan.koshal@hangarww.com>
2020-09-10 18:30:17 +0400

    [amazon-clone] implement delete product function

--- src/stateProvider/reducer.js
+++ src/stateProvider/reducer.js
@@ -5,14 +5,30 @@ export const initialState = {
 // selector
 export const getBasketTotal = (basket) => basket?.reduce((total, curr) => curr.price + total, 0);
 
-const reducer = (state = initialState, action) => {
+const reducer = (state, action) => {
     console.log(action);
+
     switch (action.type) {
         case 'ADD_TO_BASKET':
             return {
                 ...state,
                 basket: [...state.basket, action.item]
             };
+        case 'REMOVE_FROM_BASKET':
+            // this will find the first item and return it to you
+            const index = state.basket.findIndex(
+                basketItem => basketItem.id === action.id
+            );
+
+            let newBasket = [...state.basket];
+
+            if (index >= 0) {
+                //item found
+                newBasket.splice(index, 1); //remove item by index(id of item), 1: remove only one 
+            } else {
+                console.warn(`Cant remove product (id: ${action.id}) as its not in basket!`);
+            }
+            return { ...state, basket: newBasket };
         default:
             return state;
     }
