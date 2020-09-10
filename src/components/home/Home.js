9201db35e3b2c2edf3557fb23f748582126ae058
Parwan Koshal <parwan.koshal@hangarww.com>
2020-09-10 18:28:33 +0400

    [amazon] update product id's

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,16 +6,18 @@ function Home() {
     return (
         <div className="home">
             <div className="home__container" >
-                <img className="home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg" alt="banner" />
+                <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="banner" />
             </div>
 
             <div className="home__row">
-                <Product title="The lean startup"
+                <Product
+                    id="lk"
+                    title="The lean startup"
                     price={29.99}
                     image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_Electronics_1x._SY304_CB432774322_.jpg"
                     rating={5} />
                 <Product
-                    id="545645634"
+                    id="qwe"
                     title="Kenwood Kmix Stand Mixer"
                     price={239.0}
                     rating={4}
@@ -25,21 +27,21 @@ function Home() {
 
             <div className="home__row">
                 <Product
-                    id="545645634"
+                    id="dfg"
                     title="Kenwood Kmix Stand Mixer"
                     price={239.0}
                     rating={4}
                     image="https://images-eu.ssl-images-amazon.com/images/I/41TR7G8VboL._AC_US160_FMwebp_QL70_.jpg"
                 />
                 <Product
-                    id="545645634"
+                    id="xcv"
                     title="Kenwood Kmix Stand Mixer"
                     price={239.0}
                     rating={4}
                     image="https://m.media-amazon.com/images/I/61n6Ovq6EdL._AC_UY218_.jpg"
                 />
                 <Product
-                    id="545645634"
+                    id="qwe"
                     title="Kenwood Kmix Stand Mixer"
                     price={239.0}
                     rating={4}
@@ -49,14 +51,14 @@ function Home() {
 
             <div className="home__row">
                 <Product
-                    id="545645634"
+                    id="asd"
                     title="Kenwood Kmix Stand Mixer"
                     price={239.0}
                     rating={4}
                     image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355.jpg"
                 />
             </div>
-     
+
         </div >
     );
 }
