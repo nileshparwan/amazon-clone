34c1dd5471d2834289d7959e0f9b69dfeac9c114
Parwan Koshal <parwan.koshal@hangarww.com>
2020-09-10 21:24:51 +0400

    [amazon][header] refactor link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -35,15 +35,19 @@ function Header() {
                     </div>
                 </Link>
 
-                <div className="header__option">
-                    <span className="header__optionLineOne">Returns</span>
-                    <span className="header__optionLineTwo">& Orders</span>
-                </div>
+                <Link to={!user ? '/login' : "/home"}>
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Returns</span>
+                        <span className="header__optionLineTwo">& Orders</span>
+                    </div>
+                </Link>
 
-                <div className="header__option">
-                    <span className="header__optionLineOne">Your</span>
-                    <span className="header__optionLineTwo">Prime</span>
-                </div>
+                <Link to={!user ? '/login' : "/home"}>
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Your</span>
+                        <span className="header__optionLineTwo">Prime</span>
+                    </div>
+                </Link>
 
                 <Link to="/checkout">
                     <div className="header__optionBasket">
