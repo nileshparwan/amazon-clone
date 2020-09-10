13526e2ca0b778feec7250392e7b8f2089b9721b
Parwan Koshal <parwan.koshal@hangarww.com>
2020-09-10 20:55:00 +0400

    [amazon] check if user is logged in

--- src/App.js
+++ src/App.js
@@ -5,8 +5,32 @@ import Home from './components/home/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Checkout from './components/checkout/Checkout';
 import Login from './components/login/Login';
+import { auth } from './config/firebase';
+import { useStateValue } from './stateProvider/stateProvider';
+
 
 function App() {
+  const [{ }, dispatch] = useStateValue();
+
+  React.useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      console.log("THE USE IS >>>", authUser);
+      if (authUser) {
+        // user loggedin
+        dispatch({
+          type: "SET_USER",
+          user: authUser
+        });
+      } else {
+        // user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null
+        });
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <div className="app">
