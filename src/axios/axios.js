adad939ba347fdce3b8066c9404d62fde52ced9a
Parwan Koshal <parwan.koshal@hangarww.com>
2020-09-11 23:18:09 +0400

    [amazon][axios] add online web api

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/fir-82e13/us-central1/api' //the api )cloud function) url
+    baseURL: 'https://us-central1-fir-82e13.cloudfunctions.net/api' //the api )cloud function) url
 });
+//https://us-central1-fir-82e13.cloudfunctions.net/api => now this is our deploy app api (
+    //go to firebase > functions and copy the request link
+//)
+
+// http://localhost:5001/fir-82e13/us-central1/api => we use this only for local test
 
 // baseUrl ends with /api because of exports.api which is in functions/index
 
